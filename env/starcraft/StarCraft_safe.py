60e6f5ba41e2b17f3439d8b33363d0dfe5208e30
raz.shmueli <raz.shmueli@corsound.ai>
2025-10-01 14:24:37 +0300

    max 0 for

--- env/starcraft/StarCraft_safe.py
+++ env/starcraft/StarCraft_safe.py
@@ -261,6 +261,7 @@ class StarCraft(Config):
         """Cost based on NEW deaths this step only"""
         current_deaths = info.get("dead_allies", 0)
         new_deaths = current_deaths - getattr(self, "prev_deaths", 0)
+        new_deaths = max(0, new_deaths)
         self.prev_deaths = current_deaths
         return new_deaths
 
